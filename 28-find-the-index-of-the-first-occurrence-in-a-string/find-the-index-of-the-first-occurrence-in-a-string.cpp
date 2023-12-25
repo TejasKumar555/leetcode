3633130d75b37ffb57b80083aae7c90978f955ee
TejasKumar555 <123978524+TejasKumar555@users.noreply.github.com>
2023-12-25 23:46:52 +0530

    Time: 0 ms (100.00%) | Memory: 6.6 MB (87.36%) - LeetSync

---
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int strStr(string h, string n) {
+
+
+       int i = 0, j = 0;
+
+    while (h[i] != '\0' && n[j] != '\0') {
+        if (h[i] == n[j]) {
+            i++;
+            j++;
+        } else {
+            i = i - j + 1;  // Move i back to the next position in haystack
+            j = 0;         // Reset j to 0
+        }
+    }
+
+    if (n[j] == '\0')
+        return (i - j);
+
+    return -1;
+
+
+        
+    }
+};

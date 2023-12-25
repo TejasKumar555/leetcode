dc865c465719cdec182d66093229c463dcb36710
TejasKumar555 <123978524+TejasKumar555@users.noreply.github.com>
2023-12-26 00:16:04 +0530

    Time: 0 ms (100.00%) | Memory: 6.2 MB (99.46%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -5,14 +5,15 @@ public:
         return n;
     }
 
-    std::vector<int> dp(n + 1, 0);
-    dp[1] = 1;
-    dp[2] = 2;
+    int prev1 = 1;
+    int prev2 = 2;
 
     for (int i = 3; i <= n; ++i) {
-        dp[i] = dp[i - 1] + dp[i - 2];
+        int current = prev1 + prev2;
+        prev1 = prev2;
+        prev2 = current;
     }
 
-    return dp[n];
+    return prev2;
     }
 };

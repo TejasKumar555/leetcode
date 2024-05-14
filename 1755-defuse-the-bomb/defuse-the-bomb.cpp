8b5de317812ef865e3e2f6d67714d818b11c45e5
TejasKumar555 <123978524+TejasKumar555@users.noreply.github.com>
2024-05-14 13:26:35 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (26.75%) - LeetSync

--- 1755-defuse-the-bomb/defuse-the-bomb.cpp
+++ 1755-defuse-the-bomb/defuse-the-bomb.cpp
@@ -1,28 +1,38 @@
-#include <vector>
-using namespace std;
-
 class Solution {
 public:
-    vector<int> decrypt(vector<int>& code, int k) {
-        int n = code.size();
-        vector<int> decrypted(n, 0);
+    vector<int> decrypt(vector<int>& code, int k) 
+    {
+        int n=code.size();
+        vector<int>res(n,0);
         
-        if (k == 0) return decrypted; // If k is 0, all elements should be 0.
+        if(k==0)
+            return res;
         
-        for (int i = 0; i < n; ++i) {
-            int sum = 0;
-            if (k > 0) {
-                for (int j = 1; j <= k; ++j) {
-                    sum += code[(i + j) % n]; // Wrap around to the beginning if index exceeds n.
-                }
-            } else {
-                for (int j = -1; j >= k; --j) {
-                    sum += code[(i + j + n) % n]; // Wrap around to the end if index goes below 0.
-                }
-            }
-            decrypted[i] = sum;
+		// Taking a window of k elements
+		
+		// First window will be from index 1 to index k
+        int start=1,end=k;
+        if(k<0)
+        {
+		   // if k is negative then the first window will be the last k elements
+            k*=-1;
+            start=n-k;
+            end=n-1;
+        }
+        int sum=0;
+        // maintaining first window
+        for(int i=start;i<=end;i++)
+            sum+=code[i];
+        
+        for(int i=0;i<n;i++)
+        {
+            res[i]=sum;
+            // sliding the window 
+            sum-=code[(start++)%n];
+            sum+=code[(++end)%n];
         }
         
-        return decrypted;
+        return res;
+        
     }
 };

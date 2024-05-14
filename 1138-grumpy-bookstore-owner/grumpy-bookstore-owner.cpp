9ae1da5db16758dd4e444ed46f730baf63c3d38f
TejasKumar555 <123978524+TejasKumar555@users.noreply.github.com>
2024-05-15 02:55:01 +0530

    Time: 29 ms (26.71%) | Memory: 34.4 MB (44.87%) - LeetSync

--- 1138-grumpy-bookstore-owner/grumpy-bookstore-owner.cpp
+++ 1138-grumpy-bookstore-owner/grumpy-bookstore-owner.cpp
@@ -1,35 +1,35 @@
 class Solution {
 public:
     int maxSatisfied(vector<int>& customers, vector<int>& grumpy, int minutes) {
-        int n = customers.size();
-        int sum = 0, res = 0, temp = 0;
-        
-        // Calculate the initial satisfied customers when the owner is not grumpy
-        for (int i = 0; i < n; ++i) {
+
+
+        int i=0,j=0;
+        int n=customers.size();
+        int sum=0,res=0;
+        int temp=0;
+
+       for (int i = 0; i < n; i++) {
             if (!grumpy[i]) {
                 sum += customers[i];
                 customers[i] = 0; // Marking as 0 to indicate already satisfied
             }
         }
-        
-        int i = 0, j = 0;
-        while (j < n) {
-            if (grumpy[j]) {
-                temp += customers[j]; // Accumulate grumpy customers
-            }
-            // Adjusting the window size
-            if (j - i + 1 < minutes) {
-                ++j; // Expand the window
-            } else { // Window size reaches minutes
-                res = max(res, sum + temp);
-                if (grumpy[i]) {
-                    temp -= customers[i]; // Update temp if customer left the window
-                }
-                ++i; // Move the window start
-                ++j; // Move the window end
-            }
-        }
-        
+
+
+        while(j < n) {
+    if (grumpy[j])
+        temp += customers[j];
+    if (j - i + 1 < minutes)
+        j++;
+    else if (j - i + 1 == minutes) {
+        res = max(res, sum + temp);
+        if (grumpy[i])
+            temp -= customers[i];
+        i++;
+        j++;
+    }
+}
+
         return res;
     }
 };

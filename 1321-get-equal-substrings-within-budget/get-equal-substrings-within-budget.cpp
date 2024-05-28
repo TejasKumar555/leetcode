9912ced9c3c35ec7ace0f45d2a6daa8bf24c26e6
TejasKumar555 <123978524+TejasKumar555@users.noreply.github.com>
2024-05-28 23:34:57 +0530

    Time: 9 ms (41.73%) | Memory: 8.8 MB (70.28%) - LeetSync

--- 1321-get-equal-substrings-within-budget/get-equal-substrings-within-budget.cpp
+++ 1321-get-equal-substrings-within-budget/get-equal-substrings-within-budget.cpp
@@ -1,28 +1,52 @@
 class Solution {
 public:
-    int equalSubstring(string s, string t, int maxCost) {
-        int i = 0;
-        int j = 0;
-        int cost = 0;
-        int len = 0;
+    int equalSubstring(string s, string t, int maxcost) {
 
-        while (j < s.length()) {
-            // Add the cost of transforming s[j] to t[j]
-            cost += abs(s[j] - t[j]);
 
-            // If the cost exceeds maxCost, adjust the window from the left
-            while (cost > maxCost) {
-                cost -= abs(s[i] - t[i]);
-                i++;
+
+
+        int i=0;
+        int j=0;
+        int cost=0;
+        int len=0;
+
+
+
+        while(j<s.length())
+        {
+
+            cost=cost+abs(s[j]-t[j]);
+            if(cost<=maxcost)
+            {
+                len=max(len,j-i+1);
+               
+               
+                
+            }
+            else if(cost>maxcost)
+            {
+                while(cost>maxcost)
+                {
+                    cost=cost-abs(s[i]-t[i]);
+                    i++;
+                }
+
+
+             
+               
+                
             }
 
-            // Update the maximum length of the valid window
-            len = max(len, j - i + 1);
 
-            // Move the right pointer to expand the window
             j++;
+
+
+           
+            
         }
 
+
         return len;
+        
     }
 };

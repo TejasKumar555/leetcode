71a6e868c6e0503ec61f6ae49867d1a36b4a2885
TejasKumar555 <123978524+TejasKumar555@users.noreply.github.com>
2024-07-17 00:40:36 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (15.06%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
-    int rob(vector<int>& nums) {
+    int rob(vector<int>& arr) {
+       int  n=arr.size();
 
-        int n=nums.size();
-        vector<int>dp(n,0);
-        dp[0]=nums[0];
-
-        for(int i=1;i<nums.size();i++)
-        {
-
-            int pick=nums[i];
-            if(i>1)pick=pick+dp[i-2];
-            int no=dp[i-1];
-
-            dp[i]=max(pick,no);
-
-            
-        }
-
-
-        return dp[n-1];
+        int prev = arr[0];   // Initialize the maximum sum ending at the previous element
+    int prev2 = 0;       // Initialize the maximum sum ending two elements ago
+    
+    for (int i = 1; i < n; i++) {
+        int pick = arr[i];  // Maximum sum if we pick the current element
+        if (i > 1)
+            pick += prev2;  // Add the maximum sum two elements ago
+        
+        int nonPick = 0 + prev;  // Maximum sum if we don't pick the current element
+        
+        int cur_i = max(pick, nonPick);  // Maximum sum ending at the current element
+        prev2 = prev;   // Update the maximum sum two elements ago
+        prev = cur_i;   // Update the maximum sum ending at the previous element
+    }
+    
+    return prev;  
         
     }
 };

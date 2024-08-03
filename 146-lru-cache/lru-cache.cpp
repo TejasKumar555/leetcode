c2e997d59ef6310c55d77cdf57454efe734bcd92
TejasKumar555 <123978524+TejasKumar555@users.noreply.github.com>
2024-08-04 02:26:18 +0530

    Time: 363 ms (49.61%) | Memory: 183.6 MB (17.68%) - LeetSync

---
+++ 146-lru-cache/lru-cache.cpp
@@ -0,0 +1,94 @@
+class LRUCache {
+public:
+    class node{
+        public:
+        int key;
+        int val;
+        node* next;
+        node* prev;
+        node(int k,int v)
+        {
+            key=k;
+            val=v;
+        }
+    };
+
+    node* head=new node(-1,-1);
+    node* tail=new node(-1,-1);
+    int cap;
+    unordered_map<int,node*>m;
+
+    LRUCache(int capacity) {
+
+        cap=capacity;
+        head->next=tail;
+        tail->prev=head;
+
+
+        
+    }
+
+    void addnode(node* newnode)
+    {
+        node* temp=head->next;
+        temp->prev=newnode;
+        newnode->next=temp;
+        head->next=newnode;
+        newnode->prev=head;
+
+    }
+
+
+    void deletenode(node* delnode)
+    {
+        node* delprev=delnode->prev;
+        node* delnext=delnode->next;
+        delprev->next=delnext;
+        delnext->prev=delprev;
+    }
+    
+    int get(int key) {
+
+        if(m.find(key)!=m.end())
+        {
+            node* resnode=m[key];
+            int val=resnode->val;
+            m.erase(key);
+            deletenode(resnode);
+            addnode(resnode);
+            m[key]=head->next;
+            return val;
+        }
+
+        return -1;
+
+
+        
+    }
+    
+    void put(int key, int value) {
+
+        if(m.find(key)!=m.end())
+        {
+            node *exist=m[key];
+            m.erase(key);
+            deletenode(exist);
+        }
+        if(m.size()==cap)
+        {
+            m.erase(tail->prev->key);
+            deletenode(tail->prev);
+        }
+
+        addnode(new node(key,value));
+        m[key]=head->next;
+        
+    }
+};
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache* obj = new LRUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
